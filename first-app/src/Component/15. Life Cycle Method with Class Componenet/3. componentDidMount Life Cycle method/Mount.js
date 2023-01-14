6a7a5a3af3cb850bcfcf53e83a9fba734efd98a2
Imtiyazdeveloper <imtiyazmd803@gmail.com>
2023-01-14 15:56:57 +0530

    Added more files

--- first-app/src/Component/15. Life Cycle Method with Class Componenet/3. componentDidMount Life Cycle method/Mount.js
+++ first-app/src/Component/15. Life Cycle Method with Class Componenet/3. componentDidMount Life Cycle method/Mount.js
@@ -1,26 +1,36 @@
 
 import React from "react";
-import  {Component} from 'react'
+import { Component } from 'react'
+
 class Mount extends Component {
+
     constructor() {
         super();
-        console.log("constructor")
-        this.state = { name: "anil" }
+
+        this.state = { name: "Imtiyaz" }
+
+        console.log("constructor") // pahle constructor call hota hai 
     }
+
     componentDidMount() {
-        console.log("componentDidMount")
+        console.log("componentDidMount") // render method ke call hone ke baad componentDidMount call hota hai
+
 
     }
+
     render() {
-        console.log("render")
+        console.log("render") // constructor ke baad hamara render method call hota hai
 
         return (
             <div className="App">
                 <h1>Component Did Mount {this.state.name}</h1>
-                <button onClick={() => this.setState({ name: "Sidhu" })}>Update</button>
+                <button onClick={() => this.setState({ name: "Hey Imtiyaz" })}>Update</button>
+
             </div>
         );
     }
 }
 
 export default Mount;
+
+
