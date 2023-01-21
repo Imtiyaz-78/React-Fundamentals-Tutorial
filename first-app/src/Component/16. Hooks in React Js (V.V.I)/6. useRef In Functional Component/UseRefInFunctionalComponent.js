b43cd9df92da53bba57aaa38e751c0a035a91ba3
Imtiyazdeveloper <imtiyazmd803@gmail.com>
2023-01-21 14:34:55 +0530

    useRef Hook in Functional Component

--- first-app/src/Component/16. Hooks in React Js (V.V.I)/6. useRef In Functional Component/UseRefInFunctionalComponent.js
+++ first-app/src/Component/16. Hooks in React Js (V.V.I)/6. useRef In Functional Component/UseRefInFunctionalComponent.js
@@ -4,11 +4,19 @@ import React from "react";
 import { useRef } from "react";
 
 function UseRefInFunctionalComponent() {
+    const myref = useRef(null);
 
+    function myHandelr() {
+        myref.current.value = "2000";
+        myref.current.style.color = "red";
+        myref.current.focus();
+    }
 
     return (
         <div>
-            <h1>haha</h1>
+            <h1>useRef in Hook in Functional Component</h1>
+            <input type="text" ref={myref} />
+            <button onClick={myHandelr}> Handle Input</button>
         </div>
     )
 }
